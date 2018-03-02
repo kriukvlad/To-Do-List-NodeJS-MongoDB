63bdc43daf6db3fee71fdb39493eef6b9baa5951
@kriukvlad <kriukvlad@gmail.com>
2018-03-02 17:25:42 +0200

    sqldb&queryies

---
+++ mySQLDB/sql queries.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT status FROM TodoList.tasks WHERE name LIKE 'A%';
+
+SELECT COUNT(*) FROM TodoList.projects ORDER BY name; 
+
+SELECT * FROM TodoList.projects WHERE name LIKE 'G%';
+
+SELECT * FROM TodoList.projects WHERE name LIKE '%a%';
